498b006761c5bbf45d3a0ed8cfe981fee4effb76
Aetreas <anthonycaruso157@gmail.com>
2024-01-26 17:43:11 -0500

    Build Update
    - Added basic block sprites
    - Began scripting game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour //This script will handle score, lives, etc. For now, handles player relationship to ghosts and pellets//
+{
+    public Ghost[] ghost;
+    public player player;
+    public Transform pellete; 
+
+    
+}
